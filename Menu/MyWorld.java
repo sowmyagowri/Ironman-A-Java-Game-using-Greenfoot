853b7c615d5e234c2ee5309c3dfea4b10b442eb5
Rucha Apte <rucha.apte@sjsu.edu>
2017-10-24 23:14:25 -0700

    Menu Page upload

---
+++ Menu/MyWorld.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * This is the Menu for the game. It will have a Play button and a Help button
+ * 
+ * Rucha Apte(your name) 
+ * @version 1
+ */
+public class MyWorld extends World
+{
+
+    private static final String bgImageName = "2.jpg";    //menu
+
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public MyWorld()
+    {    
+        super(1500, 800, 1); 
+        setBackground(bgImageName);
+
+    }
+}
