1266560de19b5772eed5dc65548be599cfdda308
sowmyav27 <viswanathan.sowmya@gmail.com>
2017-10-25 21:51:53 -0700

    Add files via upload
    Context and Strategy classes for the Enemy Level 2 module updated

---
+++ Level 2 Enemy/Strategy.java
@@ -0,0 +1,17 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Strategy here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public interface Strategy 
+{
+    /**
+     * Act - do whatever the Strategy wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act(); 
+    public void fly();
+}
