0478b21acf7993e2c3bade5fa247353e82f57056
Sowmya Gowrishankar <gsowmya@gmail.com>
2017-10-26 21:20:36 -0700

    Created IronmanState

---
+++ Level 1 Ironman/IronmanState.java
@@ -0,0 +1,13 @@
+/**
+ * Write a description of class IronmanState here.
+ * 
+ * @author (Sowmya Gowrishankar) 
+ * @version (a version number or a date)
+ */
+
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public interface IronmanState
+{
+    public void attackEnemy();
+}
--- Level 1 Ironman/Scenario.java
+++ Level 1 Ironman/Scenario.java
@@ -3,7 +3,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class Day here.
  * 
- * @author (your name) 
+ * @author (Sowmya Gowrishankar) 
  * @version (a version number or a date)
  */
 public class Scenario extends World
