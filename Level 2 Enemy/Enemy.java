e8d68e21a1516af215cd6d0dcb10f1f1236d414b
sowmyav27 <viswanathan.sowmya@gmail.com>
2017-10-27 20:51:07 -0700

    Add files via upload
    Updating the Enemy class with the 'Dying functionality" for itself.
    yet to tweak so that it gets destroyed after 'N' hits by the player.
    B.java file is for creating an explosion kind of image.

--- Level 2 Enemy/Enemy.java
+++ Level 2 Enemy/Enemy.java
@@ -1,4 +1,5 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*;
+import java.util.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
  * Write a description of class Enemy here.
@@ -10,29 +11,51 @@ public class Enemy extends Actor
 {
    
     private int targetx=0, targety=0,jeda=0;
-    public static int dir=0;
+    public static int dir=0,lives=5;
     private int vx=1;
+    public boolean toRemove=false;
     //MainEnemy em=null;
     public void checkwalls()
     {
             if(isAtEdge()&&dir==0)
               {
-
                   setLocation(getX(),getY()-50);
                   dir=1;
                   
                 }
                 if(isAtEdge()&&dir==1)
               {
-
                   setLocation(getX(),getY()+50);
                   dir=0;
                   
                 }
             
     }
+    public void destroy()
+    {
+        getWorld().addObject(new B(),getX(),getY());
+        //System.out.println(lives);
+        //Enemylives liv = (Enemylives)(getWorld().getObjects(Enemylives.class).get(0));
+        lives--;
+        //System.out.println(lives);
+        if(lives==0)
+        toRemove=true;
+        else
+        if(lives==1)
+        toRemove=false;
+        else
+        if(lives==2)
+        toRemove=false;
+        else
+        if(lives==3)
+        toRemove=false;
+        else
+        if(lives==4)
+        toRemove=false;
+        
+    }
     
-    public void act() 
+    public void move()
     {
         if(dir==0)
         {
@@ -44,12 +67,21 @@ public class Enemy extends Actor
         setLocation(getX(),getY()-vx);
         checkwalls();
     }
-  
-        
-        if(jeda>0) jeda--;
+    if(jeda>0) jeda--;
         if(jeda%150==140) getWorld().addObject(new Fire(), getX(),getY());
         if(jeda%150==80) getWorld().addObject(new Context(new Object2()), getX(),getY());
         if(jeda==0) jeda=150;
+    }
+    public void act() 
+    {
+        
+        if(!toRemove) move();
+        else
+        if(toRemove)
+        getWorld().removeObject(this);
+        
+        
+        
     }    
 }
 
