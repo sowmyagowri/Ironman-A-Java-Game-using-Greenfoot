11589f15f53a20f3953634f6a3a1050ce040f1cc
Avni Gulati <avnigulati@hotmail.com>
2017-10-28 17:43:54 -0700

    Update MyWorld.java

--- .Level 1 Enemy/MyWorld.java
+++ .Level 1 Enemy/MyWorld.java
@@ -9,13 +9,16 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class MyWorld extends World
 {
 
+    private static final String bgImageName = "2.jpg";    //menu
+
     /**
      * Constructor for objects of class MyWorld.
      * 
      */
     public MyWorld()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(1500, 800, 1); 
+        setBackground(bgImageName);
+
     }
 }
