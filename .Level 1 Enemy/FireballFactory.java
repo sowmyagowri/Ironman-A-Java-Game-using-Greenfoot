9467f75cb0d04c1b854eac4834cc79fbd4414655
Avni Gulati <avnigulati@hotmail.com>
2017-11-01 14:14:11 -0700

    Fireball Factory class created

---
+++ .Level 1 Enemy/FireballFactory.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class FireballFactory here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class FireballFactory extends DestructionElementFactory
+{
+    /**
+     * Act - do whatever the FireballFactory wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
