615291cf779443a315e411b4689dafb35204863c
Avni Gulati <avnigulati@hotmail.com>
2017-11-06 23:58:38 -0800

    Update RocketEnemyFactory.java

--- .Level 1 Enemy/RocketEnemyFactory.java
+++ .Level 1 Enemy/RocketEnemyFactory.java
@@ -1,19 +1,20 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
 /**
- * Write a description of class RocketEnemyFactory here.
+ * Write a description of class FireballFactory here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class RocketEnemyFactory extends DestructionElementFactory
+public class RocketEnemyFactory extends DestructionElementFactory 
 {
+    // instance variables - replace the example below with your own
+    private int x;
+
     /**
-     * Act - do whatever the RocketEnemyFactory wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
+     * Constructor for objects of class FireballFactory
      */
-    public void act() 
+    public DestructionElement makeDestructionElement()
     {
-        // Add your action code here.
-    }    
+        return new RocketElement();
+    } 
 }
+
