fc4b1a2c23797b08c73b1082b0e4c8310f66ff14
Rucha Apte <rucha.apte@sjsu.edu>
2017-11-06 19:10:02 -0800

    Updated button.java

--- Menu/Button.java
+++ Menu/Button.java
@@ -12,6 +12,10 @@ public class Button extends Actor
      * Act - do whatever the Button wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+     public Button()
+    {
+    }
+    
     public void act() 
     {
         // Add your action code here.
