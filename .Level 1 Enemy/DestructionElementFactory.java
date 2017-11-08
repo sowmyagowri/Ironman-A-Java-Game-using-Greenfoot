ffa4e47f85e92b1ae678f19272956f3fbafd2d4a
Avni Gulati <avnigulati@hotmail.com>
2017-11-08 12:27:30 -0800

    Integrating Level1 enemy with Scoreboard  module

--- .Level 1 Enemy/DestructionElementFactory.java
+++ .Level 1 Enemy/DestructionElementFactory.java
@@ -6,33 +6,34 @@
  */
 public class DestructionElementFactory  
 {
+    // instance variables - replace the example below with your own
     /**
      * Constructor for objects of class DestructionElementFactory
      */
-    public DestructionElement DestructionElementFactory(int newDestructionElement)
+    public Objects selectDestructionElementFactory(int newDestructionElement,ScoreDisplay display2,ScoreDisplay display3,ScoreDisplay display4)
     {
-        DestructionElement newElement = null;
+        Objects newElement = null;
         
         if(newDestructionElement == 0)
         {
-            FireballFactory newFireballFactory = new FireballFactory();
-            newElement = newFireballFactory.makeDestructionElement();
+            FireballFactory newFireballFactory = new FireballFactory(display4);
+            newElement = newFireballFactory.makeDestructionElement(display4);
             
         }
         else if(newDestructionElement == 1)
         {
-            SpaceShipFactory newSpaceShipFactory = new SpaceShipFactory();
-            newElement = newSpaceShipFactory.makeDestructionElement();
+            SpaceShipFactory newSpaceShipFactory = new SpaceShipFactory(display2);
+            newElement = newSpaceShipFactory.makeDestructionElement(display2);
         }
         else if(newDestructionElement == 2)
         {
-            RocketFactory newRocketFactory = new RocketFactory();
-            newElement = newRocketFactory.makeDestructionElement();
+            RocketFactory newRocketFactory = new RocketFactory(display3);
+            newElement = newRocketFactory.makeDestructionElement(display3);
         }
         
         return newElement;
 
     }
- 
 }
+
 
