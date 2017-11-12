b2be110f30f54c58f357bd31a2bd62273d389b33
sowmyav27 <viswanathan.sowmya@gmail.com>
2017-11-11 18:56:51 -0800

    Add files via upload

---
+++ Level 2 Enemy/Integration with Scoreboard/Objects.java
@@ -0,0 +1,23 @@
+
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Objects here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Objects extends Actor
+{
+    //private String subjectState;
+    private ScoreBoard sb = new ScoreBoard();
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+    
+    public ScoreBoard get_sb()
+    {
+          return sb;
+    }
+}
