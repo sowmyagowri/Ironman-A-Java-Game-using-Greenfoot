9ef0c09a69d34b283330f5e7e216754c6ce25936
sowmyav27 <viswanathan.sowmya@gmail.com>
2017-11-14 19:20:38 -0800

    Updating files
    files updated with the final integration changes.

---
+++ Level 2 Enemy/Final Integration/Enemy.java
@@ -0,0 +1,121 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.*;
+
+/**
+ * Write a description of class Enemy here.
+ * 
+ * @author (Sowmya Viswanathan) 
+ * @version (a version number or a date)
+ */
+public class Enemy extends Objects
+{
+   private int counter=0,vx=1;
+   public static int dir=0;
+   public boolean toRemove=false;
+   private ScoreDisplay sd1;
+   private ScoreDisplay sd2;
+   private ScoreDisplay sd3;
+   private ScoreDisplay sd4;
+   int lives_left;
+   
+   public Enemy(ScoreDisplay score1, ScoreDisplay score2, ScoreDisplay score3, ScoreDisplay score4 )
+    {
+        this.sd1=score1;
+        this.sd2=score2;
+        this.sd3=score3;
+        this.sd4=score4;
+        lives_left = get_scoreboard().get_enemy_hit();
+    }
+   
+   public void checkwalls()
+   {
+       if(getY() == 0)
+       {
+           setLocation(getX(),getY()+50);
+           dir=1;                  
+       }
+       if(getY() == getWorld().getHeight()-1)
+       {
+           setLocation(getX(),getY()-50);
+           dir=0;                  
+       }
+   }
+   public void check_lives()
+   {
+       lives_left = get_scoreboard().get_enemy_hit();
+       if(lives_left > 0)
+       {
+           toRemove=false;
+       }
+       else 
+       {
+           toRemove=true;  
+           GameOver gameover = new GameOver();
+           getWorld().addObject(gameover, getWorld().getWidth()/2, getWorld().getHeight()/2);
+           Greenfoot.stop();
+       }
+   }
+    
+   public void move()
+   {
+   if(dir==0)
+   {
+       setLocation(getX(),getY()-vx);
+       checkwalls();
+   }
+   if(dir==1)
+   {
+       setLocation(getX(),getY()+vx);
+       checkwalls();
+   }
+   Enemy e=this;
+   World world=getWorld();
+    
+   Handler s1 = new Object1(sd2);
+   Handler s2 = new Object2(sd3);
+   Handler s3 = new Object3(sd4);
+    
+   s1.setSuccessor(s2);
+   s2.setSuccessor(s3);
+   counter=getRandomNumber(20,300);
+   if(counter==100) { s1.setObject(world,e,"OBJECT1"); }
+   if(counter==200) { s1.setObject(world,e,"OBJECT2"); }
+   if(counter==250) { s1.setObject(world,e,"OBJECT3"); }
+   }
+   public int getRandomNumber(int start,int end)
+   {
+       int normal = Greenfoot.getRandomNumber(end-start+1);
+       return normal+start;
+    }
+   public void act() 
+   {
+       check_lives();
+       if(!toRemove) 
+       {
+           move();
+           removeEnemy();
+       }
+       /*else
+       {
+            getWorld().removeObject(this);
+        }*/
+   }    
+   
+   public void removeEnemy()
+    {
+         if(isTouching(shot.class))
+         {
+            //explosionSound.play();
+            Explosion bomb = new Explosion();
+            getWorld().addObject(bomb, getX(), getY()); 
+            removeTouching(shot.class);
+            notifyObservers();
+         }
+    }
+    
+    public void notifyObservers()
+    {
+        get_scoreboard().updateScore(this);
+        sd1.updateDisplay(this);
+    }
+}
