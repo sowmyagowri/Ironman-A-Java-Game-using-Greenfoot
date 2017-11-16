ccb4a78af1cc21d133f10cda9bf01e654d1540f7
Rucha Apte <rucha.apte@sjsu.edu>
2017-11-15 20:42:00 -0800

    Add files via upload

--- Menu/ButtonHelp.java
+++ Menu/ButtonHelp.java
@@ -3,7 +3,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class ButtonHelp here.
  * 
- * @author (your name) 
+ * @author Rucha Apte 
  * @version (a version number or a date)
  */
 public class ButtonHelp extends Button implements ButtonInvoker
--- Menu/ButtonInvoker.java
+++ Menu/ButtonInvoker.java
@@ -1,5 +1,12 @@
 import greenfoot.*;
 
+/**
+ * Write a description of class ButtonInvoker here.
+ * 
+ * @author Rucha Apte 
+ * @version (a version number or a date)
+ */
+ 
 public interface ButtonInvoker
 {
         public void click();
